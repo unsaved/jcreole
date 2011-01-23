03fed0e85226bc5f171ca9c309cebbc85055c001
Blaine Simpson <blaine.simpson@admc.com>
2011-01-23 13:09:43 -0500

    Renamed parser driver program

--- src/com/admc/jcreole/CreoleParseDriver.java
+++ src/com/admc/jcreole/CreoleParseDriver.java
@@ -3,12 +3,12 @@ package com.admc.jcreole;
 import java.io.IOException;
 import java.io.File;
 
-public class CreoleDriver {
+public class CreoleParseDriver {
     public static void main(String[] sa)
             throws IOException, beaver.Parser.Exception {
         if (sa.length != 1)
             throw new IllegalArgumentException(
-                    "SYNTAX: java com.admc.play.CreoleDriver file.creole");
+                    "SYNTAX: java com.admc.play.CreoleParseDriver file.creole");
         CreoleScanner scanner =
                 CreoleScanner.newCreoleScanner(new File(sa[0]), false);
                 // Change 'false' to 'true' to silently strip bad input chars.
